46331987f32981bd2694f002583ba2de40f47de2
Oli Pfeffer <oli.pfeffer@ring.com>
2018-01-15 14:52:16 -0500

    Initial Commit

---
+++ Giphr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Giphr
+//
+//  Created by Oli Pfeffer on 1/15/18.
+//  Copyright © 2018 Oliver Pfeffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
